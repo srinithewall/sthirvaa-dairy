294b06a1359666986d6db7622ff2abaeceaf4846
srinithewall <srini.thewall@gmail.com>
2026-05-18 23:54:29 +0530

    fix(prod): Remove output standalone from next.config.ts to restore Cloudflare Pages automatic builds

--- dairy-ui/next.config.ts
+++ dairy-ui/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   /* config options here */
   eslint: {
     // Warning: This allows production builds to successfully complete even if
